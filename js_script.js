38c7e2eb46d4508a077913df093cf4188689f358
Sprou-t <150780574+Sprou-t@users.noreply.github.com>
2024-05-06 03:35:24 +0800

    added function to add objects into an array and loop thru the array to display the array items inside

--- js_script.js
+++ js_script.js
@@ -1,6 +1,5 @@
 //create a library array to store all my book objs
-let library=[]
-// [{title:'Hobbit', author:'J.R.R Tolkien', page:'250'}, {title:'Percy Jackson', author:'Rick Riordan', page:'350'}];
+let booklist=[];
 
 //obj constructor
 function Book(title,author,page){
@@ -9,34 +8,10 @@ function Book(title,author,page){
     this.page = page;
 }
 
+//hv no use for this now
 Book.prototype.info = function() {
     return `This book is ${this.title} by ${this.author}. Page: ${this.page}`;
 };
-
-//create 
-
-//take user input & store the new book obj into array
-    function add_book_to_lib(){
-        //prompt user about the book info using forms in html
-        //take input, create obj and put obj into array
-        const enterButton = document.querySelector('.submit');
-        enterButton.addEventListener('click',(e)=>{
-            e.preventDefault();
-            let input1=document.querySelector('#title');
-            let input2=document.querySelector('#author');
-            let input3=document.querySelector('#page');
-            //get all the values
-            const value1 = input1.value;
-            const value2 = input2.value;
-            const value3 = input3.value;
-            //use the values to create the obj
-            let newBook = new Book(value1,value2,value3);
-            //Add the object to the array
-            library.push(newBook);
-            console.log(library);
-            
-        })
-    }
 
 //create button-onclick event that pops up a form 
 function pop_up_form(){
@@ -46,5 +21,58 @@ function pop_up_form(){
     })
 }
 
+//take user input & store the new book obj into array
+function add_book_to_lib(){
+    //prompt user about the book info using forms in html
+    //take input, create obj and put obj into array
+    const enterButton = document.querySelector('.submit');
+    enterButton.addEventListener('click',(e)=>{
+        e.preventDefault(); //prevent form from reloading aft submitting when i press the button as my books data will be lost
+        let input1=document.querySelector('#title');
+        let input2=document.querySelector('#author');
+        let input3=document.querySelector('#page');
+        //get all the values
+        const value1 = input1.value;
+        const value2 = input2.value;
+        const value3 = input3.value;
+        //use the values to create the obj
+        let newBook = new Book(value1,value2,value3);
+        //Add the object to the array
+        booklist.push(newBook);
+
+        // // Display the books on the webpage
+         displayBooks();
+
+        // Reset the form inputs
+        document.querySelector('.form-container').reset();
+    })
+}
+
+//loop thru the array to display all the books on the webpage
+function displayBooks(){
+    let display_panel = document.querySelector('.display_panel');
+    display_panel.innerHTML = ''; // Clear previous content
+    //loop w .foreach()
+    booklist.forEach(book=>{
+        //add html content in .display_panel div
+            //create each div for each book
+        let bookdiv = document.createElement('div');
+
+            //create each para of each bk & append them to the div
+        let titlepara = document.createElement('p');
+        titlepara.textContent = `Title: ${book['title']}`;
+        bookdiv.appendChild(titlepara);
+        let authorpara = document.createElement('p');
+        authorpara.textContent = `Author: ${book.author}`;
+        bookdiv.appendChild(authorpara);
+        let pagepara = document.createElement('p');
+        pagepara.textContent = `Page number: ${book.page}`;
+        bookdiv.appendChild(pagepara);
+        //append bookdiv to display_panel
+        display_panel.appendChild(bookdiv);
+    })
+}
+
 pop_up_form();
 add_book_to_lib();
+displayBooks();
